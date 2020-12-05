b62176cb264638a98c30e3aea27d24e8cc9eced6
Biswajit Pradhan <biswajit.pradhan@nettantra.net>
2020-12-05 18:21:49 +0530

    home tab new curd add two way binding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { ClassField } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { HttpServiceService } from '../http-service.service';
 
@@ -7,17 +8,89 @@ import { HttpServiceService } from '../http-service.service';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  dataItem: any;
+  sub = true;
+  up = false;
+  id: string = '';
+  name: string = '';
+  email: string = '';
+  mobile: string = '';
 
+  showdata: any;
+  allDataPost: any;
+  allDataUpdate: any;
+
+  dataItem: any;
   constructor(private httpService: HttpServiceService) {}
   ngOnInit() {
-    this.httpService.getTag().subscribe((data) => {
-      this.dataItem = data;
+    this.httpService.getdata().subscribe((e) => {
+      let all = e;
+      this.dataItem = all;
+
       console.log(this.dataItem);
-      // data.map((item) => {
-      //   this.dataItem = item;
-      //   console.log(this.dataItem);
-      // });
+    });
+  }
+  submit(name: string, email: string, mobile: string) {
+    this.allDataPost = {
+      name: name,
+      email: email,
+      mobile: mobile,
+    };
+    console.log(this.allDataPost);
+    this.httpService.postData(this.allDataPost).subscribe((e) => {
+      console.log(e);
+      this.httpService.getdata().subscribe((e) => {
+        let all = e;
+        this.dataItem = all;
+
+        console.log(this.dataItem);
+      });
+    });
+  }
+  delete(id: number) {
+    let param = {
+      id: id,
+    };
+    this.httpService.deleteData(param).subscribe((e) => {
+      console.log(e);
+      this.httpService.getdata().subscribe((e) => {
+        let all = e;
+        this.dataItem = all;
+
+        console.log(this.dataItem);
+      });
+    });
+  }
+  edit(id: number) {
+    let param = {
+      id: id,
+    };
+    this.sub = false;
+    this.up = true;
+    this.httpService.editData(param).subscribe((e) => {
+      this.showdata = e;
+      this.id = this.showdata.id;
+      this.name = this.showdata.name;
+      this.email = this.showdata.email;
+      this.mobile = this.showdata.mobile;
+    });
+  }
+  update(name: string, email: string, mobile: string, id: string) {
+    console.log(id);
+    this.allDataUpdate = {
+      id: id,
+      name: name,
+      email: email,
+      mobile: mobile,
+    };
+
+    this.httpService.updateData(this.allDataUpdate).subscribe((e) => {
+      console.log(e);
+      this.httpService.getdata().subscribe((e) => {
+        let all = e;
+        this.dataItem = all;
+
+        console.log(this.dataItem);
+      });
     });
   }
 }
--- src/app/http-service.service.ts
+++ src/app/http-service.service.ts
@@ -2,19 +2,33 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AppModule } from './app.module';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HttpServiceService {
-  // apiurl = 'http://localhost:8000/api/addtag';
-  apiurl ='https://jsonplaceholder.typicode.com/posts';
+  geturl = 'http://localhost:8000/api/index';
+  postUrl = 'http://localhost:8000/api/create';
+  deleteUrl = 'http://localhost:8000/api/delete';
+  editUrl = 'http://localhost:8000/api/show';
+  updateUrl = 'http://localhost:8000/api/update';
 
-  constructor(private _http:HttpClient) { }
+  // apiurl ='https://jsonplaceholder.typicode.com/posts';
 
+  constructor(private _http: HttpClient) {}
 
-
-  getTag(){
-    return this._http.get<AppModule[]>(this.apiurl);
+  getdata() {
+    return this._http.get<AppModule[]>(this.geturl);
+  }
+  postData(allDataPost: any) {
+    return this._http.post<AppModule[]>(this.postUrl, allDataPost);
+  }
+  deleteData(id: any) {
+    return this._http.post<AppModule[]>(this.deleteUrl, id);
+  }
+  editData(id: any) {
+    return this._http.post<AppModule[]>(this.editUrl, id);
+  }
+  updateData(upData: any) {
+    return this._http.post<AppModule[]>(this.updateUrl, upData);
   }
 }
